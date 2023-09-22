61fc3601fd67c05a8aee2b1361314e5dc5a5865f
jaydip45 <132422781+jaydip45@users.noreply.github.com>
2023-09-22 15:37:11 +0530

    Add files via upload

---
+++ UserSchema.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const UserSchema = new Schema({
+    name: {
+        type: String
+    },
+    email: {
+        type: String
+    },
+    password: {
+        type: String
+        
+    },
+    
+    role: {
+        type: Schema.Types.ObjectId,
+        ref: "role"
+    }
+})
+
+module.exports = mongoose.model('users', UserSchema)
